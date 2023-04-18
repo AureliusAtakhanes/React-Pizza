1283b0f29f17f78bc5f649ab1cdc791286d419e8
Aurelius Atakhanes <atahanbapaev@gmail.com>
2023-04-18 13:28:29 +0600

    feat: category list rendering

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,15 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Categories = () => {
+    const [activeIndex, setActiveIndex] = useState(0)
+
+    const categories = [
+        'Все',
+        'Мясные',
+        'Вегетарианская',
+        'Гриль',
+        'Острые',
+        'Закрытые',
+    ]
+
+    const onClickCategory = (index) => {
+        setActiveIndex(index)
+    }
+
     return (
         <div className="categories">
             <ul>
-                <li className="active">Все</li>
-                <li>Мясные</li>
-                <li>Вегетарианская</li>
-                <li>Гриль</li>
-                <li>Острые</li>
-                <li>Закрытые</li>
+                {
+                    categories.map((pizza, index) => (
+                        <li
+                            onClick={() => onClickCategory(index)}
+                            className={activeIndex === index ? 'active' : ''}
+                            key={index}
+                        >
+                            {pizza}
+                        </li>
+                    ))
+                }
             </ul>
         </div>
     )
