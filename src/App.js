86e33481c1a0c05f5451e7145a1e0b9deafe564c
Aurelius Atakhanes <atahanbapaev@gmail.com>
2023-04-20 13:50:24 +0600

    feat: Getting data from the server and rendering it

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import Categories from './components/Categories';
 import Header from './components/Header';
 import PizzaBlock from './components/PizzaBlock';
@@ -5,9 +6,16 @@ import Sort from './components/Sort';
 
 import './scss/app.scss';
 
-import pizzas from './assets/pizzas.json';
-
 function App() {
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    fetch('https://6440e954792fe886a898ea59.mockapi.io/items')
+      .then((res) => res.json())
+      .then((arr) => setItems(arr))
+  }, [])
+
+
   return (
     <div className="wrapper">
       <Header />
@@ -20,7 +28,7 @@ function App() {
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
             {
-              pizzas.map((obj) => (
+              items.map((obj) => (
                 <PizzaBlock
                   {...obj} key={obj.id}
                 />
