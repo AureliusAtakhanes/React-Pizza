02594a2df06cf143eb43e8f96d7e446a76e9813b
Aurelius Atakhanes <atahanbapaev@gmail.com>
2023-04-20 14:10:01 +0600

    feat: added skeleton to project

--- src/App.js
+++ src/App.js
@@ -3,16 +3,23 @@ import Categories from './components/Categories';
 import Header from './components/Header';
 import PizzaBlock from './components/PizzaBlock';
 import Sort from './components/Sort';
+import { Skeleton } from './components/Skeleton';
 
 import './scss/app.scss';
 
 function App() {
   const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     fetch('https://6440e954792fe886a898ea59.mockapi.io/items')
       .then((res) => res.json())
-      .then((arr) => setItems(arr))
+      .then((arr) => {
+        setTimeout(() => {
+          setItems(arr)
+          setIsLoading(false)
+        }, 1000)
+      })
   }, [])
 
 
@@ -28,11 +35,9 @@ function App() {
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
             {
-              items.map((obj) => (
-                <PizzaBlock
-                  {...obj} key={obj.id}
-                />
-              ))
+              isLoading
+                ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+                : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)
             }
           </div>
         </div>
