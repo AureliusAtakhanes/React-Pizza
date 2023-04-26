219cae84f6e5f8b71a731450fb45bc112c665f2a
Aurelius Atakhanes <atahanbapaev@gmail.com>
2023-04-26 12:14:06 +0600

    feat: debugging sortPopUp

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { setSort } from '../redux/slices/filterSlice'
 
 const Sort = () => {
     const dispatch = useDispatch()
     const sort = useSelector(state => state.filter.sort)
+    const sortRef = useRef()
 
     const [open, setOpen] = useState(false)
     const list = [
@@ -21,8 +22,22 @@ const Sort = () => {
         setOpen(false)
     }
 
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (!event.path.includes(sortRef.current)) {
+                setOpen(false)
+            }
+        }
+
+        document.body.addEventListener('click', handleClickOutside)
+
+        return () => {
+            document.body.removeEventListener('click', handleClickOutside)
+        }
+    }, [])
+
     return (
-        <div className="sort">
+        <div ref={sortRef} className="sort">
             <div className="sort__label">
                 <svg
                     width="10"
